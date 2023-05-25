2ac037cce9e8c1301aa5521ab0a88edd71ea34f2
Piyush Dwivedi <121784263+aarmin9696@users.noreply.github.com>
2023-05-25 15:00:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function updateText() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+// Get the HTML element with ID "output"
+const outputElement = document.getElementById("output");
+
+// Call the updateText function and wait for the promise to resolve
+updateText().then((text) => {
+  // Update the text of the outputElement
+  outputElement.textContent = text;
+});
